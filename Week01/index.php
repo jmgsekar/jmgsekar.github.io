49bb2c45cb87a0bc255bfcab50b4e8e631555368
Gnanasekar <jmg_sekar@yahoo.com>
2020-05-20 09:01:18 +0530

    Update index.php

--- Week01/index.php
+++ Week01/index.php
@@ -44,7 +44,7 @@ if (isset($_SESSION['name']) )
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>JMG's Resume Registry</title>
+		<title>0712bc45</title>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	</head>
 	<body>
